f2e67fa930d3db10459c6e1c81cde7c410efcc40
WiseyXD <aryan.s.nag@gmail.com>
2024-04-17 23:18:13 +0530

    null day

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -24,7 +24,7 @@ export default async function Page() {
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
 
-          {/* AWS EC2 deployement */}
+          {/* Null Day */}
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">

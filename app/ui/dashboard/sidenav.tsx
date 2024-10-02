a94bc6c19e9cc98d13e1ada3f832dd25e9a2c66e
Aryan Nagbanshi <aryan.s.nag@gmail.com>
2024-10-02 11:30:13 +0530

    feat : apurti dashboard

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,7 +2,6 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import AcmeLogo from '@/app/ui/acme-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { usePathname } from 'next/navigation';
 
 export default function SideNav() {
   return (
